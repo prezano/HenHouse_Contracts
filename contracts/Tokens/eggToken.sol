96d38ae9916dc9a5caaf08c71ff2788ff463e381
gonzalo <gonzalo@ipsy.com>
2022-01-18 10:52:01 -0300

    removing extra code and fixing egg token

--- contracts/Tokens/eggToken.sol
+++ contracts/Tokens/eggToken.sol
@@ -7,12 +7,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EggToken is ERC20, Ownable {
 
-    address public addressForBosses;
     address public henHouseAddress;
     address public stakingAddress;
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        addressForBosses = _msgSender();
     }
 
     modifier onlyHenHouse() {
@@ -20,26 +18,12 @@ contract EggToken is ERC20, Ownable {
         _;
     }
 
-    modifier onlyStaker() {
-        require(stakingAddress == _msgSender(), "caller is not the staker");
-        _;
-    }
-
     function setHenHouseAddress(address _hh) public onlyOwner {
         henHouseAddress = _hh;
     }
 
-    function setStakingAddress(address _ss) public onlyOwner {
-        stakingAddress = _ss;
-    }
-
     function mintTokens(address _depositAddress, uint256 _amount) public onlyHenHouse {
         _mint(_depositAddress, _amount);
-    }
-
-    function endStaking(address _to, uint256 _amount) public onlyStaker {
-        transferFrom(msg.sender, _to, _amount);
-        _burn(_to, _amount);
     }
 
     function burn(uint256 amount) public onlyHenHouse {
